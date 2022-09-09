cbe9e9915fd6eed263a65856569a3c46f0d20ce0
Quang Tran <nhatquang304@gmail.com>
2022-09-08 23:35:05 -0400

    create a bare minimal Typescript project

---
+++ Xplore/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Welcome to SOEN490</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
