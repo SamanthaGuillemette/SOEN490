dd0d6219683a12cf0f65aa5f170d6a1c12b3fc78
Quang Tran <nhatquang304@gmail.com>
2022-09-30 09:31:59 -0400

    create custom hook to load resources (fonts, icons) in background

--- Xplore/App.tsx
+++ Xplore/App.tsx
@@ -2,25 +2,32 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider as PaperProvider } from "react-native-paper";
-import useColorScheme from "./hooks/useColorScheme";
+import { useColorScheme } from "./hooks/useColorScheme";
+import { useCachedResources } from "./hooks/useCachedResources";
 import colors from "./constants/colors";
 
 const App = () => {
   const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources();
 
   console.log(colors[colorScheme].completedPrimary);
   const bgColor: string = colors[colorScheme].completedBackground;
 
-  return (
-    <SafeAreaProvider>
-      <PaperProvider>
-        <StatusBar style="auto" />
-        <Text style={styles.text1}>Welcome to SOEN490</Text>
-        <Text style={styles.text2}>Color scheme: {colorScheme}</Text>
-        <View style={[styles.dynamicColor, { backgroundColor: bgColor }]} />
-      </PaperProvider>
-    </SafeAreaProvider>
-  );
+  if (!isLoadingComplete) {
+    // "return null" === keep displaying the splash screen
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <PaperProvider>
+          <StatusBar style="auto" />
+          <Text style={styles.text1}>Welcome to SOEN490</Text>
+          <Text style={styles.text2}>Color scheme: {colorScheme}</Text>
+          <View style={[styles.dynamicColor, { backgroundColor: bgColor }]} />
+        </PaperProvider>
+      </SafeAreaProvider>
+    );
+  }
 };
 
 export default App;
