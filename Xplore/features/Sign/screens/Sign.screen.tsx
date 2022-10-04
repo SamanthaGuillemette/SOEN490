c725f18028641b6c91ce17ca9bdb9ef2881803f5
Laila Alhalabi <laila.alhalabi99@gmail.com>
2022-10-04 14:42:27 -0400

    Created sign up screen #1

--- Xplore/features/Sign/screens/Sign.screen.tsx
+++ Xplore/features/Sign/screens/Sign.screen.tsx
@@ -1,19 +1,21 @@
 import * as React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
+import { StyleSheet, SafeAreaView, ScrollView, View } from "react-native";
 import { Solgan } from "../../../components/Solgan";
 import { SegmentedButton } from "../../../components/SegmentedButton";
 import SignIn from "./SignIn.screen";
-
+import SignUp from "./SignUp.screen";
 interface SignProps {}
 
 const Sign = ({}: SignProps) => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.sign_container}>
-        <Solgan />
-        <SegmentedButton />
-        <SignIn />
-      </View>
+      <ScrollView>
+        <View style={styles.sign_container}>
+          <Solgan />
+          <SegmentedButton />
+          <SignUp />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
---
+++ Xplore/features/Sign/screens/SignUp.screen.tsx
@@ -0,0 +1,132 @@
+import * as React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+interface SignUpProps {}
+
+const SignUp = ({}: SignUpProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputsContainer}>
+        <View style={styles.inputWrapper}>
+          <Image
+            style={styles.textInputIcon}
+            source={require("../../../assets/icons/UserIcon.png")}
+          />
+          <TextInput placeholderTextColor={"#C4C4C4"} placeholder="Username" />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Image
+            style={styles.textInputIcon}
+            source={require("../../../assets/icons/email.png")}
+          />
+          <TextInput placeholderTextColor={"#C4C4C4"} placeholder="Email" />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Image
+            style={styles.textInputIcon}
+            source={require("../../../assets/icons/Password.png")}
+          />
+          <TextInput
+            placeholderTextColor={"#C4C4C4"}
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Image
+            style={styles.textInputIcon}
+            source={require("../../../assets/icons/Password.png")}
+          />
+          <TextInput
+            placeholderTextColor={"#C4C4C4"}
+            secureTextEntry={true}
+            placeholder="Confirm Password"
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.login_button}
+          //onPress={() => navigation.navigate("UserGuide")}
+        >
+          <Text style={styles.inputTextStyle}>SIGN IN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.forgot_button}
+          //onPress={() => navigation.navigate("UserGuide")}
+        >
+          <Text style={styles.inputTextStyleDark}>FORGOT PASSWORD</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  inputsContainer: {
+    marginTop: 50,
+  },
+  inputWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#C4C4C4",
+    padding: 5,
+    marginBottom: 25,
+    width: 250,
+  },
+  textInputIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 10,
+  },
+  login_button: {
+    backgroundColor: "#463FB0",
+    marginTop: 50,
+    width: 250,
+    paddingVertical: 16,
+    borderRadius: 25,
+    shadowColor: "#463FB0",
+    shadowOpacity: 0.3,
+    elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+  },
+  forgot_button: {
+    width: 250,
+    marginTop: 15,
+    marginBottom: 45,
+    paddingVertical: 16,
+    borderRadius: 25,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#463FB0",
+    shadowColor: "#463FB0",
+    shadowOpacity: 0.1,
+    elevation: 3,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+  },
+  inputTextStyle: {
+    textAlign: "center",
+    color: "white",
+  },
+  inputTextStyleDark: {
+    textAlign: "center",
+    color: "#463FB0",
+  },
+});
