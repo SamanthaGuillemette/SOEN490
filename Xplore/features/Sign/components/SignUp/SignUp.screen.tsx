5c85c288305f27c5e9013fe4105168b106d2ec5b
Tushar Raval <tushar.raval102@gmail.com>
2022-10-13 22:27:40 -0400

    #1 - added some comments on what to implement

--- Xplore/features/Sign/components/SignUp/SignUp.screen.tsx
+++ Xplore/features/Sign/components/SignUp/SignUp.screen.tsx
@@ -3,17 +3,28 @@ import { PrimaryButton, View, TextInput } from "../../../../components";
 import styles from "./SignUp.styles";
 import { account } from "../../../../assets/appwrite/appwrite";
 
+// Need to implement
+//  ** error handling
+//    ** maybe a popup window or toast
+//    ** for stuff below:
+//  ** input sanitization
+//    ** Passwods are the same
+//    ** Username does not exist
+//    ** email does not exist
+//    ** email is valid (xyz@gmail.com)
+//  ** getting a name
+
 const SignUp = () => {
   const [username, setUserName] = React.useState<string>("");
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
-  console.log(username + " " + email + " " + password + " ");
   function SignUpFunction() {
     const promise = account.create(username, email, password, username);
     promise.then(
       function (result: any) {
         console.log("result: \n");
         console.log(result);
+        // TODO Redirect if all good to dashboard
       },
       function (err: any) {
         console.log("error: \n");
