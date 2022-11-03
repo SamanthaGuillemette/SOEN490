c65c7e72a23f77c976ce34883653f2c32784e557
Quang Tran <nhatquang304@gmail.com>
2022-11-03 08:59:07 -0400

    XP #3: update .gitignore file & disable code coverage

--- Xplore/components/Text/Text.test.js
+++ Xplore/components/Text/Text.test.js
@@ -1,9 +1,21 @@
 import { render } from "@testing-library/react-native";
 import { Text } from "./Text.component";
 
+// Describe the test group (component)
 describe("Testing <Text /> component", () => {
+  // Describe the test case
   it("renders correctly", () => {
     const textComponent = render(<Text variant="h1">Hello</Text>);
     expect(textComponent.getByText("Hello")).toBeTruthy();
   });
+
+  // You can have multiple test cases here:
+  // it("test case name", () => {
+  //   Your test code...
+  // })
+
+  // You can also choose the word "test" instead of "it":
+  // test("test case name", () => {
+  //   Your test code...
+  // })
 });
