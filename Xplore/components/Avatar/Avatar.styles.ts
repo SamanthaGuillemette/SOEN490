783765b6529f6cfb09140af833aa4c04256b455c
Quang Tran <nhatquang304@gmail.com>
2022-11-04 12:53:26 -0400

    XP #219: adjust <Avatar /> to display shadow clearly

--- Xplore/components/Avatar/Avatar.styles.ts
+++ Xplore/components/Avatar/Avatar.styles.ts
@@ -3,7 +3,9 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   avatarContainer: {
     borderRadius: 100,
-    borderWidth: 3,
+    borderWidth: 1,
     borderColor: "#fff",
+    padding: 3, // padding to shows the shadow clearly
+    marginRight: -3, // marginRight to adjust the position to the right
   },
 });
