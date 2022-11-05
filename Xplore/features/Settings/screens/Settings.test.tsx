230e51b53b99fba9c46d631369813c2e77843487
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-11-04 23:59:13 -0400

    XP #2 : Fixed settings unit test

--- Xplore/features/Settings/screens/Settings.test.tsx
+++ Xplore/features/Settings/screens/Settings.test.tsx
@@ -1,8 +1,9 @@
 import { render } from "@testing-library/react-native";
-import Settings from "./Settings.screen";
+import Settings from "../screens/Settings.screen";
 
 describe("Settings />", () => {
   it("renders correctly", () => {
     const container = render(<Settings />);
     expect(container).toBeTruthy();
   });
+});
