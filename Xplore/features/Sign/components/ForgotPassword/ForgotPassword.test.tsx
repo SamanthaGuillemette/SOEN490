7c318adb7d368da82499adf94546f71f7dbb7ab6
Lucas Catchlove <lucascatchlove@gmail.com>
2022-11-05 23:04:56 -0400

    XP #194: added unit tests for 'Reset Password' and 'Forgot Password'. Onboarding test updated to fix a bug

--- Xplore/features/Sign/components/ForgotPassword/ForgotPassword.test.tsx
+++ Xplore/features/Sign/components/ForgotPassword/ForgotPassword.test.tsx
@@ -1,3 +1,4 @@
+import { render } from "react-native-testing-library";
 import renderer from "react-test-renderer";
 import ForgotPassword from "./ForgotPassword.screen";
 
@@ -5,5 +6,25 @@ describe("Forgot Pasword screen", () => {
   it("renders correctly", () => {
     const tree = renderer.create(<ForgotPassword />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("renders title", () => {
+  it("renders the correct screen title", () => {
+    const { queryByText } = render(<ForgotPassword />);
+    expect(queryByText("Forgot password?")).not.toBeNull();
+  });
+});
+describe("renders email input box", () => {
+  it("renders the 'Email' input box", () => {
+    const { queryByPlaceholder } = render(<ForgotPassword />);
+    expect(queryByPlaceholder("Email")).not.toBeNull();
+  });
+});
+
+describe("renders the reset button", () => {
+  it("renders the correct button text", () => {
+    const { queryByText } = render(<ForgotPassword />);
+    expect(queryByText("SEND RESET LINK")).not.toBeNull();
   });
 });
--- Xplore/features/Sign/components/ResetPassword/ResetPassword.test.tsx
+++ Xplore/features/Sign/components/ResetPassword/ResetPassword.test.tsx
@@ -1,10 +1,30 @@
-import renderer from "react-test-renderer";
+// Hello.spec.js
+import React from "react";
+import { render } from "react-native-testing-library";
 import ResetPassword from "./ResetPassword.screen";
 
-describe("Reset Pasword screen", () => {
-  jest.useFakeTimers();
-  it("renders correctly", () => {
-    const tree = renderer.create(<ResetPassword />).toJSON();
-    expect(tree).toMatchSnapshot();
+describe("renders title", () => {
+  it("renders the correct screen title", () => {
+    const { queryByText } = render(<ResetPassword />);
+    expect(queryByText("Reset password")).not.toBeNull();
+  });
+});
+
+describe("renders password input boxes", () => {
+  it("renders the 'password' input box", () => {
+    const { queryByPlaceholder } = render(<ResetPassword />);
+    expect(queryByPlaceholder("Password")).not.toBeNull();
+  });
+
+  it("renders the 'Re-enter password' input box", () => {
+    const { queryByPlaceholder } = render(<ResetPassword />);
+    expect(queryByPlaceholder("Re-enter password")).not.toBeNull();
+  });
+});
+
+describe("renders the reset button", () => {
+  it("renders the correct button text", () => {
+    const { queryByText } = render(<ResetPassword />);
+    expect(queryByText("RESET PASSWORD")).not.toBeNull();
   });
 });
