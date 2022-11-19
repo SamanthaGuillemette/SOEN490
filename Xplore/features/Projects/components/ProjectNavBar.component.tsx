78411cedd82246cb1ce55a30413fb81d7eba1297
Faizan Ahmad <51469062+AndroidBiscuit@users.noreply.github.com>
2022-11-19 11:52:38 -0500

    #165 Transferred project nav bar

---
+++ Xplore/features/Projects/components/ProjectNavBar.component.tsx
@@ -0,0 +1,148 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Dimensions,
+  FlatList,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  //TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
+import ProjectDescription from "./ProjectDescription.component";
+import Links from "./Links.component";
+import { useThemeColor } from "../../../hooks";
+import { colors } from "../../../constants";
+//import { useThemeColor } from "../../../hooks";
+
+//const tabBarBackground = useThemeColor("backgroundSecondary");
+
+interface ScrollableTabViewPager {
+    children: React.ReactNode;
+    shadowOffset?: number;
+    backgroundColor?: keyof typeof colors.light & keyof typeof colors.dark;
+    lightColor?: string;
+    darkColor?: string;
+    style?: StyleProp<ViewStyle>;
+  }
+
+const { width } = Dimensions.get("window");
+
+const headers = ["Description", "Tasks", "Discussion", "Members", "Links"];
+
+const projectScreenPages = [
+  <ProjectDescription />,
+  <ProjectDescription />,
+  <ProjectDescription />,
+  <ProjectDescription />,
+  <Links />,
+];
+
+export default function ScrollableTabViewPager() {
+  const [active, setActive] = useState(0);
+  const headerScrollView = useRef();
+  const itemScrollView = useRef();
+  const {
+    backgroundColor = "backgroundSecondary",
+    lightColor,
+    darkColor,
+    style,
+  } = props;
+  useEffect(() => {
+    headerScrollView.current.scrollToIndex({
+      index: active,
+      viewPosition: 0.5,
+    });
+  }, [active]);
+  const onPressHeader = (index: React.SetStateAction<number>) => {
+    itemScrollView.current.scrollToIndex({ index });
+    setActive(index);
+  };
+  const onMomentumScrollEnd = (e) => {
+    const newIndex = Math.round(e.nativeEvent.contentOffset.x / width);
+    if (active != newIndex) {
+      setActive(newIndex);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={headers}
+        ref={headerScrollView}
+        keyExtractor={(item) => item}
+        horizontal
+        style={styles.headerScroll}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item, index }) => (
+          <View>
+            <TouchableHighlight
+              onPress={() => onPressHeader(index)}
+              //underlayColor={tabBarBackground}
+              key={item}
+              style={[
+                styles.headerItem,
+                // eslint-disable-next-line react-native/no-inline-styles
+                { backgroundColor: active == index ? "#0cc" : backgroundColor },
+              ]}
+            >
+              <Text>{item}</Text>
+            </TouchableHighlight>
+            {active == index && <View style={styles.headerBar} />}
+          </View>
+        )}
+      />
+      <FlatList
+        data={headers}
+        ref={itemScrollView}
+        keyExtractor={(item) => item}
+        horizontal
+        pagingEnabled
+        decelerationRate="fast"
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={onMomentumScrollEnd}
+        renderItem={({ item, index }) => (
+          <View key={item} style={styles.mainItem}>
+            {projectScreenPages[index]}
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    marginTop: -20,
+    marginLeft: -20,
+    width: width,
+  },
+  headerScroll: {
+    flexGrow: 0,
+    color: "white",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  headerItem: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  mainItem: {
+    width: width,
+    borderWidth: 5,
+    borderColor: "#0000",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  headerBar: {
+    height: 2,
+    width: "90%",
+    alignSelf: "center",
+    // bottom bar color
+    backgroundColor: "#eedddd",
+    position: "absolute",
+    bottom: 0,
+  },
+});
