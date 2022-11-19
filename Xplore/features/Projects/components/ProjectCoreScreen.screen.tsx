1abded6560f1897fe2e55ab5b3aeb2bf76910453
Faizan Ahmad <51469062+AndroidBiscuit@users.noreply.github.com>
2022-11-19 12:20:44 -0500

    Added Projects screen to Main.tsx

--- Xplore/features/Projects/components/ProjectCoreScreen.screen.tsx
+++ Xplore/features/Projects/components/ProjectCoreScreen.screen.tsx
@@ -2,8 +2,13 @@ import { View } from "../../../components";
 //import ProjectDropDown from "./DropDown.component";
 import ProjectTitle from "./ProjectTitle.component";
 import ProjectNavBar from "./ProjectNavBar.component";
+import { NavigationProp } from "@react-navigation/native";
 
-const ProjectCoreScreen = () => {
+interface ProjectCoreScreenProps {
+  navigation: NavigationProp<any>;
+}
+
+const ProjectCoreScreen = (props: ProjectCoreScreenProps) => {
   return (
     <View backgroundColor="background">
       <ProjectTitle />
