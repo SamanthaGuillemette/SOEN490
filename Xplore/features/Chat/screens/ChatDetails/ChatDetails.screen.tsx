30efef7ecc6e349d41fd95c9f24cb0bb99e84b17
Laila Alhalabi <laila.alhalabi99@gmail.com>
2022-11-19 21:50:32 -0500

    [fix] edited imports

--- Xplore/features/Chat/screens/ChatDetails/ChatDetails.screen.tsx
+++ Xplore/features/Chat/screens/ChatDetails/ChatDetails.screen.tsx
@@ -2,7 +2,7 @@ import { NavigationProp } from "@react-navigation/native";
 import { useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native";
 import { useThemeColor } from "../../../../hooks/useThemeColor";
-import ChatDetailsHeader from "../../components/ChatDetailsHeader/ChatDetailsHeader.comonent";
+import ChatDetailsHeader from "../../components/ChatDetailsHeader/ChatDetailsHeader.component";
 import Conversation from "../../components/Conversation/Conversation.component";
 import ChatTextInput from "../../components/ChatTextInput/ChatTextInput.component";
 import styles from "./ChatDetails.styles";
