c5c31ba1178e32a5f1edef4c1d0f235672e01866
hodansh <hodanourbakhsh@icloud.com>
2022-11-22 20:42:32 -0500

    XP #168: [Fix] Removed the buttons and used AddButton base component instead.

--- Xplore/features/Projects/components/Links.component.tsx
+++ Xplore/features/Projects/components/Links.component.tsx
@@ -1,37 +1,13 @@
-import {
-  ScrollView,
-  View,
-  StyleSheet,
-  Button,
-  TouchableHighlight,
-} from "react-native";
+import { ScrollView, View, StyleSheet } from "react-native";
 import { Text, Icon } from "../../../components";
+import { AddButton } from "../../../components/AddButton";
 import { colors } from "../../../constants";
 import { useThemeColor } from "../../../hooks";
-import { useState } from "react";
 
 interface LinksProps {}
 
 export const Links = (props: LinksProps) => {
   const whiteBackground = useThemeColor("backgroundSecondary");
-  const primary = useThemeColor("primary");
-
-  const [isPressed, setPressed] = useState(true);
-  const [isPressed2, setPressed2] = useState(true);
-  const [isPressed3, setPressed3] = useState(true);
-  const touchButton1 = {
-    activeOpacity: 1,
-    style: isPressed ? styles.buttonNormal : styles.buttonPressed,
-  };
-  const touchButton2 = {
-    activeOpacity: 1,
-    style: isPressed2 ? styles.buttonNormal : styles.buttonPressed,
-  };
-  const touchButton3 = {
-    activeOpacity: 1,
-    style: isPressed3 ? styles.buttonNormal : styles.buttonPressed,
-  };
-
   return (
     <ScrollView>
       <View
@@ -53,16 +29,7 @@ export const Links = (props: LinksProps) => {
           Description
         </Text>
         <View style={{ flex: 1 }}>
-          <TouchableHighlight {...touchButton1}>
-            <Button
-              onPress={() => {
-                setPressed(false);
-              }}
-              disabled={!isPressed}
-              title={isPressed ? "ADD" : "ADDED"}
-              color="white"
-            />
-          </TouchableHighlight>
+          <AddButton />
         </View>
       </View>
       <View
@@ -84,16 +51,7 @@ export const Links = (props: LinksProps) => {
           Description
         </Text>
         <View style={{ flex: 1 }}>
-          <TouchableHighlight {...touchButton2}>
-            <Button
-              onPress={() => {
-                setPressed2(false);
-              }}
-              disabled={!isPressed2}
-              title={isPressed2 ? "ADD" : "ADDED"}
-              color="white"
-            />
-          </TouchableHighlight>
+          <AddButton />
         </View>
       </View>
       <View
@@ -115,16 +73,7 @@ export const Links = (props: LinksProps) => {
           Description
         </Text>
         <View style={{ flex: 1 }}>
-          <TouchableHighlight {...touchButton3}>
-            <Button
-              onPress={() => {
-                setPressed3(false);
-              }}
-              disabled={!isPressed3}
-              title={isPressed3 ? "ADD" : "ADDED"}
-              color="white"
-            />
-          </TouchableHighlight>
+          <AddButton />
         </View>
       </View>
     </ScrollView>
