51b4d08b68c67a31743f617e0471c8e20957d432
Faizan Ahmad <51469062+AndroidBiscuit@users.noreply.github.com>
2022-11-25 09:25:53 -0500

    [Refactor] Simplified core screen code

--- Xplore/features/Projects/components/ProjectCoreScreen.component.tsx
+++ Xplore/features/Projects/components/ProjectCoreScreen.component.tsx
@@ -1,20 +1,11 @@
 import { View } from "../../../components";
-//import ProjectDropDown from "./DropDown.component";
-import ProjectTitle from "./ProjectTitle.component";
 import ProjectNavBar from "./ProjectNavBar.component";
-import { NavigationProp } from "@react-navigation/native";
-import { useState } from "react";
+import ShadowImage from ".//ProjectComponents/ShadowImage.component";
 
-interface ProjectCoreProps {
-  navigation: NavigationProp<any>;
-}
-
-const ProjectCore = (props: ProjectCoreProps) => {
-  const { navigation } = props;
-  const [screen, setScreen] = useState(0);
+const ProjectCore = () => {
   return (
     <View backgroundColor="background">
-      <ProjectTitle />
+      <ShadowImage projectName="Snake Robot" source="" />
       <View backgroundColor="background">
         <ProjectNavBar />
       </View>
