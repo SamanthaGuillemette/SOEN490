ecd0ca38a6294ee5f254c600ba4ccd0a501736a7
hodansh <hodanourbakhsh@icloud.com>
2022-11-25 20:09:52 -0500

    XP #256: [Update] Added the task card component for meeting.

--- Xplore/features/Projects/components/Tasks/Tasks.component.tsx
+++ Xplore/features/Projects/components/Tasks/Tasks.component.tsx
@@ -41,6 +41,37 @@ export const Tasks = (props: TasksProps) => {
           />
         </View>
       </View>
+
+      <View backgroundColor="generalGray" style={styles.backgroundBox}>
+        <View backgroundColor="backgroundSecondary" style={styles.innerBox}>
+          <View style={styles.line}>
+            <LinkButton>Meeting</LinkButton>
+            <Icon style={{ marginLeft: 180 }} size="small" name="help-circle" />
+          </View>
+
+          <Text variant="h2" color="titleText">
+            {" "}
+            Finish App UI{" "}
+          </Text>
+          <Text variant="smBody" color="bodyText">
+            <Icon name="calendar" size="small" /> 13/12/2022
+          </Text>
+        </View>
+        <View style={styles.icons}>
+          <Icon
+            style={{ marginTop: -50 }}
+            color="error"
+            size="large"
+            name="trash-2"
+          />
+          <Icon
+            style={{ marginTop: -70 }}
+            color="success"
+            size="large"
+            name="check-square"
+          />
+        </View>
+      </View>
     </ScrollView>
   );
 };
