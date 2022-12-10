f0b4e731ac7a88fa25244243cf87af4a5401d45a
salehatrq <68137308+salehatrq@users.noreply.github.com>
2022-12-10 16:44:24 -0500

    XP #30: Added selected overlay with checkmark component

--- Xplore/features/TopicSelection/components/TopicCard/TopicCard.component.tsx
+++ Xplore/features/TopicSelection/components/TopicCard/TopicCard.component.tsx
@@ -1,5 +1,7 @@
-import { ImageBackground } from "react-native";
+import { useState } from "react";
+import { ImageBackground, TouchableOpacity, Image, View as RNView } from "react-native";
 import { ShadowView, Text, View } from "../../../../components";
+import { useThemeColor } from "../../../../hooks";
 import styles from "./TopicCard.styles";
 
 interface TopicCardProps {
@@ -8,21 +10,34 @@ interface TopicCardProps {
 }
 
 const TopicCard = (props: TopicCardProps) => {
+  const success = useThemeColor("success");
   const { topicName, imageURL } = props;
+  const [showImage, setShowImage] = useState(true);
+
+  const handleShowImage= () => {
+    setShowImage(!showImage)
+  }
 
   return (
-    <ShadowView backgroundColor="primary" style={styles.container}>
-      <ImageBackground
-        source={{ uri: `${imageURL}` }}
-        imageStyle={styles.imageBgContainer}
-      >
-        <View style={styles.topicNameContainer}>
-          <Text variant="h3" style={[styles.whiteText, styles.projectName]}>
-            {topicName}
-          </Text>
-        </View>
-      </ImageBackground>
-    </ShadowView>
+    <TouchableOpacity onPress={() => handleShowImage()}>
+      <ShadowView backgroundColor="primary" style={styles.container}>
+          <ImageBackground
+              source={{ uri: `${imageURL}` }}
+              imageStyle={[styles.imageBgContainer, showImage ? {borderColor: success, borderWidth: 3} : null]}
+          >
+              <View style={styles.topicNameContainer}>
+                <RNView style={showImage ? styles.overlay : null} />
+                {showImage ? (
+                  <Image
+                    source={require("../../../../assets/check-circle.png")}
+                    style={styles.checkCircleImg}/> ) : null}
+                  <Text variant="h3" color={showImage ? "smallText":  "generalGray"} style={styles.projectName}>
+                      {topicName}
+                  </Text>
+              </View>
+          </ImageBackground>
+      </ShadowView>
+    </TouchableOpacity>
   );
 };
 
--- Xplore/features/TopicSelection/components/TopicCard/TopicCard.styles.ts
+++ Xplore/features/TopicSelection/components/TopicCard/TopicCard.styles.ts
@@ -16,12 +16,28 @@ export default StyleSheet.create({
   topicNameContainer: {
     padding: 30,
   },
-  whiteText: {
-    color: "white",
+  overlay: {
+    borderRadius: 8,
+    width: deviceScreenWidth - 70,
+    height: 115,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    backgroundColor: "rgba(0,0,0,0.6)",
   },
   projectName: {
     marginBottom: 30,
     textAlign: "center",
     top: 15
   },
+  checkCircleImg: {
+    width: 24,
+    height: 24,
+    position: 'absolute', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    top: 45, 
+    left: 149,
+    zIndex: 1
+  }
 });
