f6eb9394e9f986e689024c5b53ea1e673ba0f061
salehatrq <68137308+salehatrq@users.noreply.github.com>
2023-01-06 00:21:45 -0500

    XP #30: [Fix] Fixed click outside topic card

--- Xplore/features/TopicSelection/components/TopicCard/TopicCard.component.tsx
+++ Xplore/features/TopicSelection/components/TopicCard/TopicCard.component.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ImageBackground, TouchableOpacity, Image, View as RNView } from "react-native";
-import { ShadowView, Text, View } from "../../../../components";
+import { Text, View } from "../../../../components";
 import { useThemeColor } from "../../../../hooks";
 import styles from "./TopicCard.styles";
 
@@ -20,27 +20,24 @@ export const TopicCard = (props: TopicCardProps) => {
   }
 
   return (
-    <TouchableOpacity onPress={() => handleShowImage()}>
-      <ShadowView 
-        backgroundColor="primary" 
-        isInnerShadow={false}
-        style={styles.container}>
-          <ImageBackground
-              source={{ uri: `${imageURL}` }}
-              imageStyle={styles.imageBgContainer}
-          >
-              <View style={styles.topicNameContainer}>
-                <RNView style={showImage ? [styles.selectedOverlay, {borderColor: success, borderWidth: 3}] : styles.overlay} />
-                {showImage ? (
-                  <Image
-                    source={require("../../../../assets/check-circle.png")}
-                    style={styles.checkCircleImg}/> ) : null}
-                  <Text variant="h3" color={showImage ? "smallText":  "generalGray"} style={styles.projectName}>
-                    {topicName}
-                  </Text>
-              </View>
-          </ImageBackground>
-      </ShadowView>
+    <TouchableOpacity 
+      style={styles.container}
+      onPress={() => handleShowImage()}>
+        <ImageBackground
+            source={{ uri: `${imageURL}` }}
+            imageStyle={styles.imageBgContainer}
+        >
+          <View style={styles.topicNameContainer}>
+            <RNView style={showImage ? [styles.selectedOverlay, {borderColor: success, borderWidth: 3}] : styles.overlay} />
+            {showImage ? (
+              <Image
+                source={require("../../../../assets/check-circle.png")}
+                style={styles.checkCircleImg}/> ) : null}
+              <Text variant="h3" color={showImage ? "smallText":  "generalGray"} style={styles.projectName}>
+                {topicName}
+              </Text>
+          </View>
+        </ImageBackground>
     </TouchableOpacity>
   );
 };
