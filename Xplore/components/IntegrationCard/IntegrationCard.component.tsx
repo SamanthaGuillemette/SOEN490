04cae7bf1d72103e6ad944151e4b45d85e298a1b
Marwa Khalid <khalidmarwa786@gmail.com>
2023-01-20 02:59:32 -0500

    XP #298: Added integration card

--- Xplore/components/IntegrationCard/IntegrationCard.component.tsx
+++ Xplore/components/IntegrationCard/IntegrationCard.component.tsx
@@ -1,21 +1,23 @@
-import * as React from "react";
 import { Text } from "../Text";
 import { View } from "../View";
 import { ShadowView } from "../ShadowView";
 import { Button } from "../Button";
+import { BrandIcon } from "../BrandIcon";
 import styles from "./IntegrationCard.styles";
 import { useThemeColor } from "../../hooks";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 interface IntegrationCardProps {
   testID: string;
   title: string;
   description: string;
+  iconName: keyof typeof FontAwesome5.glyphMap;
   btnType: "add" | "added";
   bgColor: "white" | "blue";
 }
 
 export const IntegrationCard = (props: IntegrationCardProps) => {
-  const { title, description, btnType, bgColor } = props;
+  const { title, description, btnType, bgColor, iconName } = props;
   const success = useThemeColor("success");
 
   return (
@@ -32,6 +34,11 @@ export const IntegrationCard = (props: IntegrationCardProps) => {
           <Text variant="h3" color="titleText" style={styles.alignTitle}>
             {title}
           </Text>
+          <BrandIcon
+            name={iconName}
+            color="primaryBackgroundOpaque"
+            style={styles.alignIcon}
+          />
           <Text variant="smBody" color="bodyText" style={styles.alignDesc}>
             {description}
           </Text>
--- Xplore/features/Projects/components/LinksScreen/Links.component.tsx
+++ Xplore/features/Projects/components/LinksScreen/Links.component.tsx
@@ -1,33 +1,32 @@
-import { ScrollView } from "react-native-gesture-handler";
 import { View, IntegrationCard } from "../../../../components";
-import styles from "./Links.styles";
 
 export const Links = () => {
   return (
-    <View style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <IntegrationCard
-          testID={"Proj_GitHubIntegration"}
-          title={"GitHub Integration"}
-          description={"Description"}
-          bgColor={"white"}
-          btnType={"add"}
-        />
-        <IntegrationCard
-          testID={"Proj_JiraIntegration"}
-          title={"Jira Integration"}
-          description={"Description"}
-          bgColor={"blue"}
-          btnType={"added"}
-        />
-        <IntegrationCard
-          testID={"Proj_FigmaIntegration"}
-          title={"Figma Integration"}
-          description={"Description"}
-          bgColor={"white"}
-          btnType={"add"}
-        />
-      </ScrollView>
+    <View>
+      <IntegrationCard
+        testID={"ProjCreation_GitHubIntegration"}
+        title={"GitHub Integration"}
+        description={"Description"}
+        iconName="github"
+        bgColor={"white"}
+        btnType={"add"}
+      />
+      <IntegrationCard
+        testID={"ProjCreation_JiraIntegration"}
+        title={"Jira Integration"}
+        description={"Description"}
+        iconName="jira"
+        bgColor={"blue"}
+        btnType={"added"}
+      />
+      <IntegrationCard
+        testID={"ProjCreation_FigmaIntegration"}
+        title={"Figma Integration"}
+        description={"Description"}
+        iconName="figma"
+        bgColor={"white"}
+        btnType={"add"}
+      />
     </View>
   );
 };
