c8d7439f6cd2130e5243fbe5353e013ad5758576
Samantha Guillemette <samantha.guillemette@gmail.com>
2023-01-25 13:40:20 -0500

    XP #282: created AddMember screen, no components

---
+++ Xplore/features/ProjectCreation/screens/AddMembers/screens/AddMembers.screen.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView } from "react-native";
+import styles from "./AddMembers.styles";
+//import { Input } from "../components";
+
+const AddMembers = () => {
+  return (
+    <SafeAreaView style={styles.safeAreaStyle}>
+      {/* Needed components */}
+    </SafeAreaView>
+  );
+};
+
+export default AddMembers;
