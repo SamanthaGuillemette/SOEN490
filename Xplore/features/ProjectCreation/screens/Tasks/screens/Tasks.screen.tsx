4be5f75a517ce4178b588286748c9917f2efb09c
Samantha Guillemette <samantha.guillemette@gmail.com>
2023-01-26 17:19:22 -0500

    XP #281: Integrated SuccessButton in Tasks creation screen

--- Xplore/features/ProjectCreation/screens/Tasks/screens/Tasks.screen.tsx
+++ Xplore/features/ProjectCreation/screens/Tasks/screens/Tasks.screen.tsx
@@ -1,11 +1,13 @@
 import { SafeAreaView } from "react-native";
 import styles from "./Tasks.styles";
 import { Input } from "../components";
+import SuccessButton from "../../../../../components/SuccessButton/SuccessButton.component";
 
 const Tasks = () => {
   return (
     <SafeAreaView style={styles.safeAreaStyle}>
       <Input />
+      <SuccessButton title="ADD" />
     </SafeAreaView>
   );
 };
