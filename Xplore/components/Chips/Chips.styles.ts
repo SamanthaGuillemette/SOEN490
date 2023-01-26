fd39c7219b0657b603e1b39681f9241312dba6e7
Marwa Khalid <khalidmarwa786@gmail.com>
2023-01-25 23:07:51 -0500

    XP #285: Fixed chip input code

--- Xplore/components/Chips/Chips.styles.ts
+++ Xplore/components/Chips/Chips.styles.ts
@@ -1,28 +1,11 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  customTag: {
-    paddingVertical: 2,
-    paddingHorizontal: 8,
-    marginRight: 10,
-    marginBottom: 10,
-    height: 36,
-    borderRadius: 8,
-    borderWidth: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    top: 4,
-  },
   alignText: {
     marginLeft: 10,
   },
   ShadowView: {
     borderRadius: 4,
     borderWidth: 1,
-  },
-  InputField: {
-    padding: 1,
-    borderRadius: 4,
   },
 });
