b271d8e1439093f793b0e639283e16a647e96863
Samantha Guillemette <samantha.guillemette@gmail.com>
2023-01-26 17:17:44 -0500

    XP #288: created base component SuccessButton

---
+++ Xplore/components/SuccessButton/SuccessButton.component.tsx
@@ -0,0 +1,58 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+import { Button, Icon } from "../";
+import styles from "./SuccessButton.styles";
+import { useState } from "react";
+import { useThemeColor } from "../../hooks";
+
+interface SuccessButtonProps extends TouchableOpacityProps {
+  title: string;
+  style?: StyleProp<ViewStyle>;
+}
+
+const SuccessButton = (props: SuccessButtonProps) => {
+  const { title, style } = props;
+  const [selected, setSelected] = useState(false);
+
+  const primary = useThemeColor("primary");
+  const bg = useThemeColor("primaryBackground");
+  return (
+    <View style={styles.alignBtn}>
+      {selected === false ? (
+        <Button
+          style={styles.btn}
+          backgroundColor="primaryBackground"
+          textColor="primary"
+          borderColor="primary"
+          onPress={() => setSelected(!selected)}
+        >
+          {title}
+        </Button>
+      ) : (
+        <TouchableOpacity
+          style={[
+            style,
+            styles.btn,
+            styles.check,
+            { backgroundColor: bg, borderColor: primary },
+          ]}
+          onPress={() => setSelected(!selected)}
+        >
+          <Icon
+            name="check-circle"
+            color="primary"
+            size="medium"
+            style={styles.checkIcon}
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default SuccessButton;
---
+++ Xplore/components/SuccessButton/SuccessButton.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  alignBtn: {
+    alignItems: "center",
+    marginTop: 23,
+  },
+  btn: {
+    width: 300,
+    height: 50,
+    borderRadius: 50,
+  },
+  check: {
+    alignItems: "center",
+    borderWidth: 1,
+  },
+  checkIcon: {
+    marginTop: 15,
+  },
+});
