9bd1f781c2cc33693de945c40a32dad15b404713
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-01-26 20:00:01 -0500

    XP #10: Fixed styling for AllTasks screen

--- Xplore/features/ProjectEdit/screens/AllTasks/screens/AllTasks.screen.tsx
+++ Xplore/features/ProjectEdit/screens/AllTasks/screens/AllTasks.screen.tsx
@@ -2,34 +2,39 @@ import { ScrollView } from "react-native";
 import { SquaredAddButton, Text, View } from "../../../../../components";
 import { TaskCard } from "../../../../../components";
 import styles from "./AllTasks.styles";
-// import { ShadowView } from "../../../../../components";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ShadowView } rom "../../../../../components";
 
 export const AllTasks = () => {
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
+    <SafeAreaView style={styles.safeAreaStyle}>
       <View style={styles.alignLeft}>
         <Text color="titleText" variant="h3">
           Click here to select a task
         </Text>
       </View>
-      <View style={styles.content}>
-        <TaskCard
-          taskType="Design"
-          taskName="UX Brainstorm"
-          taskDate="13/12/2022"
-        />
-        <TaskCard
-          taskType="Meeting"
-          taskName="Finish App UI"
-          taskDate="13/12/2022"
-        />
-        <TaskCard
-          taskType="Meeting"
-          taskName="Spring Meeting"
-          taskDate="13/12/2022"
-        />
+      <View style={styles.container}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={styles.content}>
+            <TaskCard
+              taskType="Design"
+              taskName="UX Brainstorm"
+              taskDate="13/12/2022"
+            />
+            <TaskCard
+              taskType="Meeting"
+              taskName="Finish App UI"
+              taskDate="13/12/2022"
+            />
+            <TaskCard
+              taskType="Meeting"
+              taskName="Spring Meeting"
+              taskDate="13/12/2022"
+            />
+          </View>
+        </ScrollView>
+        <SquaredAddButton />
       </View>
-      <SquaredAddButton />
-    </ScrollView>
+    </SafeAreaView>
   );
 };
