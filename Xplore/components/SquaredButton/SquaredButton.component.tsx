b3b6ff8e599504ef39e53c1b6ae299653a54cde4
Laila Alhalabi <laila.alhalabi99@gmail.com>
2023-01-29 16:42:46 -0500

    [Fix] fixed logout click

--- Xplore/components/SquaredButton/SquaredButton.component.tsx
+++ Xplore/components/SquaredButton/SquaredButton.component.tsx
@@ -7,12 +7,18 @@ import styles from "./SquaredButton.styles";
 
 interface SquaredButtonProps {
   iconName: keyof typeof Feather.glyphMap;
+  onPress: any;
 }
+
 export const SquaredButton = (props: SquaredButtonProps) => {
   const primaryBackground = useThemeColor("primaryBackground");
+  const { ...restOfProps } = props;
   return (
     <ShadowView style={[styles.button, { backgroundColor: primaryBackground }]}>
-      <TouchableOpacity style={[{ backgroundColor: primaryBackground }]}>
+      <TouchableOpacity
+        style={[{ backgroundColor: primaryBackground }]}
+        {...restOfProps}
+      >
         <Icon
           name={props.iconName}
           size="large"
