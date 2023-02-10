8e49e3d7b28d9d478219d6ddd0ed2d16788bff03
Lucas Catchlove <lucascatchlove@gmail.com>
2023-02-10 00:51:34 -0500

    added first hooks for the projects portion of the database api

--- Xplore/services/api/projects.ts
+++ Xplore/services/api/projects.ts
@@ -1,3 +1,22 @@
-import { useQuery, useMutation, QueryClientProvider, } from "react-query";
-import { database } from "../appwrite/api";
+import { useQuery, useMutation } from "react-query";
+import { PROJECT_COLLECTION_ID } from "@env";
+import api from "../appwrite/api";
 
+//to be defined
+interface Project {}
+
+const useListProjects = () => {
+  return useQuery("projects", () => api.listDocuments(PROJECT_COLLECTION_ID));
+};
+
+const useCreateProject = (data: Project) => {
+  return useMutation(() => api.createDocument(PROJECT_COLLECTION_ID, data));
+};
+
+const useDeleteProject = (documentId: string) => {
+  return useMutation(() =>
+    api.deleteDocument(PROJECT_COLLECTION_ID, documentId)
+  );
+};
+
+export { useCreateProject, useDeleteProject, useListProjects };
