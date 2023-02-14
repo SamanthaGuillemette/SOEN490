11b311248f749e0b7375a54a211dee7fea81f018
Julie Manirath <juliemanirath@gmail.com>
2023-02-13 21:27:30 -0500

    [Fix]: XP #359 test to fix topmargin for iOS

--- Xplore/features/ProjectCRUD/screens/Description/components/EditImage/EditImage.styles.ts
+++ Xplore/features/ProjectCRUD/screens/Description/components/EditImage/EditImage.styles.ts
@@ -1,9 +1,11 @@
 import { StyleSheet } from "react-native";
+import { deviceScreenHeight } from "../../../../../../constants";
 
 export default StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 0.05 * deviceScreenHeight,
   },
   circle: {
     width: 127,
