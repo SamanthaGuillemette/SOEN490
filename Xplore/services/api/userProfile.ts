82d207a61c2d6e6fa56b79146d75a2e82d0bfb6f
Lucas Catchlove <lucascatchlove@gmail.com>
2023-03-01 17:52:54 -0500

    created user profile service module. added user details fetching hook

--- Xplore/services/api/userProfile.ts
+++ Xplore/services/api/userProfile.ts
@@ -1,4 +1,17 @@
-/*
-where all the various services related to the user profile.
-Such services might include "getProfileById", and other related functions
-*/
+import {
+  useInfiniteQuery,
+  useMutation,
+  useQueryClient,
+  useQuery,
+} from "react-query";
+import { USER_COLLECTION_ID } from "@env";
+import api from "../appwrite/api";
+
+const useFetchUserDetails = async () => {
+  const userId = (await api.getAccount()).$id;
+  return useQuery({
+    queryKey: ["userDetails", userId],
+    queryFn: () =>
+      api.getDocument(USER_COLLECTION_ID, api.query.equal("userId", userId)),
+  });
+};
