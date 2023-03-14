b54bbc6a75c54c7be7c30cff2186cba881e74454
salehatrq <68137308+salehatrq@users.noreply.github.com>
2023-03-14 02:57:02 -0400

    XP #401: Moved onboarding logic to AppStack

--- Xplore/features/Onboarding/screens/Onboarding.screen.tsx
+++ Xplore/features/Onboarding/screens/Onboarding.screen.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import React, { useRef } from "react";
 import { Animated, Image, ScrollView, SafeAreaView } from "react-native";
 import { Text, View, TextButton, PrimaryButton } from "../../../components";
@@ -7,11 +6,6 @@ import { PagingDot } from "../components/PagingDot.component";
 import { NavigationProp } from "@react-navigation/native";
 import { useThemeColor } from "../../../hooks";
 import { deviceScreenWidth } from "../../../constants";
-import api from "../../../services/appwrite/api";
-import { useQuery } from "react-query";
-import { Query } from "appwrite";
-import { COLLECTION_ID_ONBOARDING } from "@env";
-import BottomTabNavigator from "../../../navigation/BottomTabNavigator";
 
 const onboardingImages = [
   {
@@ -43,121 +37,88 @@ interface OnboardingProps {
 const Onboarding = (props: OnboardingProps) => {
   const { navigation } = props;
   const whiteBackground = useThemeColor("backgroundSecondary");
-  var onboardingSeen = false;
 
   const scrollValue = useRef(new Animated.Value(0)).current;
   const translateX = scrollValue.interpolate({
     inputRange: [0, deviceScreenWidth],
     outputRange: [0, 18], // 18 = margin + width of dot = (5+5) + 8
   });
-
-  // Current user's data
-  const { data: userdata } = useQuery("user data", () => api.getAccount());
-  let userId: string = userdata?.$id as string;
-
-  const { data: onboardingData } = useQuery("onboarding data", () =>
-    api.listDocuments(COLLECTION_ID_ONBOARDING, [
-      Query.equal("userID", userId),
-    ])
-  );
-
-  if(onboardingData?.total != 0 && userId){
-    onboardingSeen = true;
-  }
-
-  useEffect(() => {
-    const getOnboarding = async () => {
-      try {
-        const onboarding_response = await api.listDocuments(
-          COLLECTION_ID_ONBOARDING,
-          [Query.equal("userID", userId)]
-        );
-        if(onboarding_response.total == 0 && userId){
-          api.createDocument(COLLECTION_ID_ONBOARDING, {userID: userId, seen: true});
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    getOnboarding();
-  }, [userId]);
   
   return (
-    onboardingSeen ? <BottomTabNavigator/> :
-      <SafeAreaView
-        style={[styles.safeAreaContainer, { backgroundColor: whiteBackground }]}
-      >
-        <View backgroundColor="backgroundSecondary" style={styles.mainContainer}>
-          <ScrollView
-            horizontal
-            pagingEnabled
-            decelerationRate="fast"
-            showsHorizontalScrollIndicator={false}
-            scrollEventThrottle={32}
-            onScroll={Animated.event(
-              [{ nativeEvent: { contentOffset: { x: scrollValue } } }],
-              { useNativeDriver: false }
-            )}
-          >
-            {onboardingImages.map((item, index) => (
-              <View
-                backgroundColor="backgroundSecondary"
-                key={index}
-                style={styles.singleSlide}
-              >
-                <Image source={item.img} style={styles.image} />
-                <Text variant="h2" style={styles.onboardingText}>
-                  {item.message}
-                </Text>
+    <SafeAreaView
+      style={[styles.safeAreaContainer, { backgroundColor: whiteBackground }]}
+    >
+      <View backgroundColor="backgroundSecondary" style={styles.mainContainer}>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={32}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollValue } } }],
+            { useNativeDriver: false }
+          )}
+        >
+          {onboardingImages.map((item, index) => (
+            <View
+              backgroundColor="backgroundSecondary"
+              key={index}
+              style={styles.singleSlide}
+            >
+              <Image source={item.img} style={styles.image} />
+              <Text variant="h2" style={styles.onboardingText}>
+                {item.message}
+              </Text>
 
-                {/* If it's the last slide, show the "Get Started" button. NOTE: will be replaced with primary button */}
-                {index === onboardingImages.length - 1 && (
-                  <View
-                    backgroundColor="backgroundSecondary"
-                    style={styles.getStartedButton}
-                  >
-                    <PrimaryButton
-                      label="Get Started"
-                      onPress={() => navigation.navigate("TopicSelection")}
-                    />
-                  </View>
-                )}
-              </View>
-            ))}
-          </ScrollView>
-
-          <View
-            backgroundColor="backgroundSecondary"
-            style={styles.bottomContainer}
-          >
-            <View backgroundColor="backgroundSecondary">
-              <View
-                backgroundColor="backgroundSecondary"
-                style={styles.dotContainer}
-              >
-                {onboardingImages.map((_, index) => (
-                  <PagingDot
-                    color="generalGray"
-                    translateX={translateX}
-                    key={index}
+              {/* If it's the last slide, show the "Get Started" button. NOTE: will be replaced with primary button */}
+              {index === onboardingImages.length - 1 && (
+                <View
+                  backgroundColor="backgroundSecondary"
+                  style={styles.getStartedButton}
+                >
+                  <PrimaryButton
+                    label="Get Started"
+                    onPress={() => navigation.navigate("TopicSelection")}
                   />
-                ))}
-              </View>
+                </View>
+              )}
+            </View>
+          ))}
+        </ScrollView>
 
-              <PagingDot
-                color="primary"
-                translateX={translateX}
-                isActive={true}
-                style={styles.activeDot}
-              />
+        <View
+          backgroundColor="backgroundSecondary"
+          style={styles.bottomContainer}
+        >
+          <View backgroundColor="backgroundSecondary">
+            <View
+              backgroundColor="backgroundSecondary"
+              style={styles.dotContainer}
+            >
+              {onboardingImages.map((_, index) => (
+                <PagingDot
+                  color="generalGray"
+                  translateX={translateX}
+                  key={index}
+                />
+              ))}
             </View>
 
-            <TextButton onPress={() => navigation.navigate("TopicSelection")}>
-              SKIP
-            </TextButton>
+            <PagingDot
+              color="primary"
+              translateX={translateX}
+              isActive={true}
+              style={styles.activeDot}
+            />
           </View>
+
+          <TextButton onPress={() => navigation.navigate("TopicSelection")}>
+            SKIP
+          </TextButton>
         </View>
-      </SafeAreaView>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- Xplore/navigation/AppStack.tsx
+++ Xplore/navigation/AppStack.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ChatDetails from "../features/Chat/screens/ChatDetails/ChatDetails.screen";
 import ChatSettings from "../features/Chat/screens/ChatSettings/ChatSettings.screen";
@@ -5,22 +6,57 @@ import Completion from "../features/Completion/screens/Completion/Completion.scr
 import LevelUp from "../features/Completion/screens/LevelUp/LevelUp.screen";
 import Onboarding from "../features/Onboarding/screens/Onboarding.screen";
 import TopicSelection from "../features/TopicSelection/screen/TopicSelection.screen";
-import BottomTabNavigator from "./BottomTabNavigator";
+import BottomTabNavigator from "./BottomTabNavigator/BottomTabNavigator";
+import api from "../services/appwrite/api";
+import { useQuery } from "react-query";
+import { Query } from "appwrite";
+import { COLLECTION_ID_ONBOARDING } from "@env";
 
 const Stack = createNativeStackNavigator();
 
 const AppStack = () => {
+  const [onboarding, setOnboarding] = useState<any | false>(false);
+
+  // Current user's data
+  const { data: userdata } = useQuery("user data", () => api.getAccount());
+  let userId: string = userdata?.$id as string;
+
+  useEffect(() => {
+    const getOnboarding = async () => {
+      try {
+        const onboarding_response = await api.listDocuments(
+          COLLECTION_ID_ONBOARDING,
+          [Query.equal("userID", userId)]
+        );
+        if(onboarding_response.total == 0 && userId){
+          api.createDocument(COLLECTION_ID_ONBOARDING, {userID: userId, seen: true});
+        }
+        else {
+          setOnboarding(
+            onboarding_response?.documents?.at(0)
+          );
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getOnboarding();
+  }, [userId]);
+
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
-      initialRouteName="Onboarding"
     >
-      <Stack.Screen name="Onboarding" component={Onboarding} />
+      {onboarding == undefined ? 
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+        :
+        <Stack.Screen name="ConditionalBottomTabNavigator" component={BottomTabNavigator} />
+      }
+      <Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} />
       <Stack.Screen name="Completion" component={Completion} />
       <Stack.Screen name="LevelUp" component={LevelUp} />
       <Stack.Screen name="ChatDetails" component={ChatDetails} />
       <Stack.Screen name="ChatSettings" component={ChatSettings} />
-      <Stack.Screen name="BottomTabNavigator" component={BottomTabNavigator} />
       <Stack.Screen name="TopicSelection" component={TopicSelection} />
     </Stack.Navigator>
   );
