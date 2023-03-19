46e46a78b7c9729a84ef32cf66853159117c2ecd
Laila Alhalabi <laila.alhalabi99@gmail.com>
2023-03-19 10:46:16 -0400

    XP #429: created chat participents avatars function

--- Xplore/services/api/chatSettings.ts
+++ Xplore/services/api/chatSettings.ts
@@ -2,8 +2,10 @@ import {
   COLLECTION_ID_GROUP_CHATS,
   COLLECTION_ID_DIRECT_CHATS,
   COLLECTION_ID_MESSAGES,
+  COLLECTION_ID_USERS,
 } from "@env";
 import { Query } from "appwrite";
+import { useState, useEffect } from "react";
 import api from "../appwrite/api";
 
 const updateLastMessage = async (chatID: any, chatType: any) => {
@@ -65,4 +67,45 @@ const changeChatName = async (nameData: any, chatID: any) => {
   }
 };
 
-export { changeChatName, deleteMessages };
+const useListAvatars = (chatID: any, chatType: any) => {
+  const [avatars, setAvatars] = useState<{ [key: string]: string }>({});
+  useEffect(() => {
+    const fetchAvatars = async () => {
+      try {
+        const collectionId =
+          chatType === "group"
+            ? COLLECTION_ID_GROUP_CHATS
+            : COLLECTION_ID_DIRECT_CHATS;
+
+        const response = await api.listDocuments(collectionId, [
+          Query.equal("chatID", chatID),
+        ]);
+        const avatarPromises = response?.documents.map(async (doc: any) => {
+          const userID = doc.userID;
+          const userResponse = await api.listDocuments(COLLECTION_ID_USERS, [
+            Query.equal("userID", userID),
+          ]);
+          const username = userResponse?.documents[0]?.username || "";
+          const avatar = userResponse?.documents[0]?.profilePicture || "";
+          return { userID, username, avatar };
+        });
+        const avatarResults = await Promise.all(avatarPromises);
+        const avatarsObject = avatarResults.reduce(
+          (obj: any, item: any) => ({
+            ...obj,
+            [item.userID]: { username: item.username, avatar: item.avatar },
+          }),
+          {}
+        );
+        setAvatars(avatarsObject);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchAvatars();
+  }, [chatID, chatType]);
+
+  return avatars;
+};
+
+export { changeChatName, deleteMessages, useListAvatars };
