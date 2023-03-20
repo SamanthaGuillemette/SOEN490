40f6968b0489a4024ecd6a497acaa52a23a753cf
Lucas Catchlove <lucascatchlove@gmail.com>
2023-03-20 15:28:00 -0400

    added DB tools to branch from

---
+++ Xplore/databaseManagementTools/projects_database_seed.js
@@ -0,0 +1,193 @@
+const projects = [
+  {
+    name: "Curlify",
+    description:
+      "A tool that automatically generates CSS code for creating unique and stylish text effects.",
+    category: "Web Development",
+    startDate: "2022-02-19 09:00:00",
+    endDate: "2022-08-19 09:00:00",
+    percentComplete: 44,
+    goals: [
+      "Develop a user-friendly interface for users to enter text and customize styling options",
+      "Implement algorithms for generating unique and creative text effects",
+      "Integrate with existing CSS libraries to provide additional styling options",
+      "Optimize the tool's performance by minimizing the amount of generated code",
+      "Provide detailed documentation and support for users to learn about CSS and text styling",
+    ],
+  },
+  {
+    name: "Cryptohub",
+    description:
+      "A platform for securely buying, selling, and trading cryptocurrency.",
+    category: "Finance/Blockchain",
+    startDate: "2022-02-19 09:00:00",
+    endDate: "2022-08-19 09:00:00",
+    percentComplete: 89,
+    goals: [
+      "Implement secure authentication and encryption protocols to protect user data",
+      "Integrate with major cryptocurrency exchanges to provide access to a wide range of trading options",
+      "Develop a user-friendly interface for users to buy, sell, and trade cryptocurrency",
+      "Provide real-time market data and analysis to help users make informed trading decisions",
+      "Implement automated trading algorithms to help users optimize their trading strategies",
+    ],
+  },
+  {
+    name: "DataChomp",
+    description:
+      "A data analytics tool that provides real-time insights and visualization for large datasets.",
+    category: "Data Science",
+    startDate: "2022-02-19 09:00:00",
+    endDate: "2022-08-19 09:00:00",
+    percentComplete: 54,
+    goals: [
+      "Implement machine learning algorithms to automatically identify trends and anomalies in large datasets",
+      "Develop a user-friendly interface for users to upload and analyze their data",
+      "Provide real-time visualization and insights to help users understand their data",
+      "Integrate with popular data sources such as SQL databases and CSV files",
+      "Optimize the tool's performance by analyzing user data and feedback",
+    ],
+  },
+  {
+    name: "Cryptosuite",
+    description:
+      "A suite of cryptography tools for encrypting and decrypting messages and files.",
+    category: "Security",
+    startDate: "2023-03-01 00:00:00",
+    endDate: "2023-06-01 00:00:00",
+    percentComplete: 11,
+    goals: [
+      "Learn about common cryptographic algorithms and how they work.",
+      "Develop an understanding of public and private key encryption.",
+      "Understand how to securely store and transmit encrypted data.",
+      "Learn about different types of attacks on cryptographic systems.",
+      "Develop practical experience in implementing cryptography tools.",
+    ],
+  },
+  {
+    name: "Streamine",
+    description:
+      "A web app for streaming and sharing music playlists with friends.",
+    category: "Web Development",
+    startDate: "2023-04-01 00:00:00",
+    endDate: "2023-09-01 00:00:00",
+    percentComplete: 35,
+    goals: [
+      "Gain practical experience in building a web app using modern web development tools.",
+      "Learn about APIs for music streaming and sharing services.",
+      "Develop skills in front-end and back-end development.",
+      "Understand how to implement user authentication and authorization.",
+      "Learn about best practices for deploying and scaling web apps.",
+    ],
+  },
+  {
+    name: "Codefyer",
+    description:
+      "A code optimization tool that analyzes and optimizes code for performance and efficiency.",
+    category: "Software Optimization",
+    startDate: "2023-05-01 00:00:00",
+    endDate: "2023-08-01 00:00:00",
+    percentComplete: 74,
+    goals: [
+      "Develop an understanding of common code optimization techniques.",
+      "Learn how to use profiling and benchmarking tools to measure code performance.",
+      "Gain practical experience in analyzing and optimizing code.",
+      "Understand how to write optimized code that is maintainable and readable.",
+      "Develop an appreciation for the trade-offs between performance and readability in software development.",
+    ],
+  },
+  {
+    name: "Simulife",
+    description:
+      "A simulation game that allows players to simulate life in a virtual world.",
+    category: "Game Development",
+    startDate: "2023-06-01 00:00:00",
+    endDate: "2023-11-01 00:00:00",
+    percentComplete: 94,
+    goals: [
+      "Develop practical experience in game design and development.",
+      "Understand the basics of game physics and mechanics.",
+      "Gain practical experience in building interactive user interfaces.",
+      "Develop an understanding of the psychology of gaming and user engagement.",
+      "Learn about best practices for game testing and deployment.",
+    ],
+  },
+  {
+    name: "CodeBites",
+    description:
+      "A social platform for sharing code snippets and getting feedback from other developers.",
+    category: "Social Networking",
+    startDate: "2022-02-19 09:00:00",
+    endDate: "2022-08-19 09:00:00",
+    percentComplete: 24,
+    goals: [
+      "Implement user authentication and authorization",
+      "Add a search feature for finding code snippets by keyword",
+      "Integrate with a syntax highlighting library for improved code readability",
+      "Implement commenting and liking functionality",
+      "Improve performance and scalability by optimizing database queries",
+    ],
+  },
+  {
+    name: "ReactoBot",
+    description: "A chatbot made with React for customer service inquiries.",
+    category: "web",
+    startDate: "2023-03-01 09:00:00",
+    endDate: "2023-04-30 17:00:00",
+    percentComplete: 55,
+    goals: [
+      "Understand how to create chatbots with React.",
+      "Learn how to integrate chatbots with customer service systems.",
+      "Develop user-friendly interfaces for chatbots.",
+      "Improve knowledge of front-end development.",
+      "Enhance problem-solving skills in programming.",
+    ],
+  },
+  {
+    name: "BlockChainz",
+    description: "A blockchain-based system for secure document verification.",
+    category: "security",
+    startDate: "2023-02-15 10:00:00",
+    endDate: "2023-06-15 18:00:00",
+    percentComplete: 66,
+    goals: [
+      "Understand how blockchain technology works.",
+      "Learn how to develop smart contracts.",
+      "Implement secure document verification using blockchain.",
+      "Gain knowledge in cryptography and security.",
+      "Improve problem-solving skills in programming.",
+    ],
+  },
+  {
+    name: "PyxieCopter",
+    description:
+      "A Python-based drone flight controller for aerial photography.",
+    category: "embedded",
+    startDate: "2023-05-01 08:00:00",
+    endDate: "2023-08-30 16:00:00",
+    percentComplete: 76,
+    goals: [
+      "Understand how to control drones with Python.",
+      "Learn how to program autonomous flight paths.",
+      "Implement real-time image processing for aerial photography.",
+      "Gain knowledge in embedded systems and control theory.",
+      "Improve problem-solving skills in programming.",
+    ],
+  },
+  {
+    name: "DataFlowz",
+    description: "A web-based data visualization tool for large datasets.",
+    category: "data",
+    startDate: "2023-04-01 11:00:00",
+    endDate: "2023-08-31 19:00:00",
+    percentComplete: 34,
+    goals: [
+      "Understand how to work with large datasets in web development.",
+      "Learn how to create interactive data visualizations.",
+      "Implement data filtering and sorting functionality.",
+      "Gain knowledge in data analysis and visualization techniques.",
+      "Improve problem-solving skills in programming.",
+    ],
+  },
+];
+
+export { projects };
