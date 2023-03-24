0201d6540169a8922de7a2373b623efcb5a059e0
salehatrq <68137308+salehatrq@users.noreply.github.com>
2023-03-23 22:53:18 -0400

    XP #434: Added additional categories to topic selection

--- Xplore/constants/categories.ts
+++ Xplore/constants/categories.ts
@@ -1,4 +1,4 @@
-export enum interests {
+export enum categories {
   frontendDev = "Frontend Development",
   backendDev = "Backend Development",
   webDev = "Web Development",
--- Xplore/features/TopicSelection/components/SelectTopics/SelectTopics.component.tsx
+++ Xplore/features/TopicSelection/components/SelectTopics/SelectTopics.component.tsx
@@ -3,6 +3,7 @@ import { View, SearchBar, NextButton } from "../../../../components";
 import { TopicCard } from "../TopicCard";
 import { TopicSelectionHeader } from "../TopicSelectionHeader";
 import { NavigationProp } from "@react-navigation/native";
+import { categories } from "../../../../constants";
 import styles from "./SelectTopics.styles";
 
 interface SelectTopicsProps {
@@ -18,33 +19,63 @@ interface TopicCardType {
 const TopicCards: TopicCardType[] = [
   {
     id: "1",
-    name: "Frontend Development",
+    name: categories.frontendDev,
     image:
       "https://meadowsideosteopathy.co.uk/wp-content/uploads/2019/12/nicole-wolf-CZ9AjMGKIFI-unsplash-768x512.jpg",
   },
   {
     id: "2",
-    name: "Backend Development",
+    name: categories.backendDev,
     image:
-      "https://cdn.bitbytesoft.com/wp-content/uploads/2022/08/Downloader.la-630c417a981e1.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO3HY7FVQlFhJyuh0fDg_5rfm8dbsKYiHiVQ&usqp=CAU",
   },
   {
     id: "3",
-    name: "DS & Algorithms",
+    name: categories.webDev,
     image:
-      "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202109/Algorithms.jpg?itok=lcIsBOBQ",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXf8uaiSiz8Ja4Ecrdnq3dWA7U-zR0otZEt7G1KHqcFcz18YTe2Sc9yiYWArmWlzXPGJQ&usqp=CAU",
   },
   {
     id: "4",
-    name: "Mobile Development",
+    name: categories.mobileDev,
     image:
       "https://cdn.sanity.io/images/s7xbv9bz/production/240237dd4b6db2858d650c1e7783b1c6dc095e90-5569x3212.jpg?rect=0,0,5569,3211&w=2560&h=1476&auto=format&fm=webp",
   },
   {
     id: "5",
-    name: "Security",
+    name: categories.gameDev,
     image:
-      "https://blog.bizvibe.com/wp-content/uploads/2020/06/top-10-security-companies.jpg",
+      "https://www.chetu.com/img/blogs/game-development-trends/game-development-trends-banner.jpg",
+  },
+  {
+    id: "6",
+    name: categories.embeddedSystems,
+    image:
+      "https://www.electronicsforu.com/wp-contents/uploads/2016/07/1743079040.jpg",
+  },
+  {
+    id: "7",
+    name: categories.algorithms,
+    image:
+      "https://news.mit.edu/sites/default/files/styles/news_article__image_gallery/public/images/202109/Algorithms.jpg?itok=lcIsBOBQ",
+  },
+  {
+    id: "8",
+    name: categories.softwareOptimization,
+    image:
+      "https://ioe.engin.umich.edu/wp-content/uploads/sites/7/2021/05/Optimization-2200px-1.png",
+  },
+  {
+    id: "9",
+    name: categories.security,
+    image:
+      "https://prod-discovery.edx-cdn.org/media/course/image/188025ff-27aa-4355-882a-507d2cdef797-e9d29e88b32c.small.png",
+  },
+  {
+    id: "10",
+    name: categories.blockchain,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQThTglOXmmVev-5vHvHTh4iRDE3NqusVLdeqPDTw8pJ_secXd4Kb5XYp9HVpwP7QalLxc&usqp=CAU",
   },
 ];
 
