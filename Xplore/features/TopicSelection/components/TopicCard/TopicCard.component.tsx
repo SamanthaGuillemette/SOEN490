9f04f7558fc4dd149f1aadf9602fe7ee0ed4bf46
salehatrq <68137308+salehatrq@users.noreply.github.com>
2023-03-24 02:52:40 -0400

    XP #435: Set state of categories based on press

--- Xplore/features/TopicSelection/components/TopicCard/TopicCard.component.tsx
+++ Xplore/features/TopicSelection/components/TopicCard/TopicCard.component.tsx
@@ -8,6 +8,7 @@ import {
 import { Text, View } from "../../../../components";
 import { useThemeColor } from "../../../../hooks";
 import styles from "./TopicCard.styles";
+import { categories } from "../../../../constants";
 
 interface TopicCardProps {
   topicName: string;
@@ -19,15 +20,43 @@ export const TopicCard = (props: TopicCardProps) => {
   const success = useThemeColor("success");
   const { topicName, imageURL } = props;
   const [showImage, setShowImage] = useState(false);
+  
+  // The different categories
+  const [frontendDev, setFrontendDev] = useState(false);
+  const [backendDev, setBackendDev] = useState(false);
+  const [webDev, setWebDev] = useState(false);
+  const [mobileDev, setMobileDev] = useState(false);
+  const [gameDev, setGameDev] = useState(false);
+  const [embeddedSystems, setEmbeddedSystems] = useState(false);
+  const [algorithms, setAlgorithms] = useState(false);
+  const [softwareOptimization, setSoftwareOptimization] = useState(false);
+  const [security, setSecurity] = useState(false);
+  const [blockchain, setBlockchain] = useState(false);
 
   const handleShowImage = () => {
     setShowImage(!showImage);
   };
 
+  const handleSetCategory = () => {
+    if (topicName == categories.frontendDev) { setFrontendDev(!frontendDev) }
+    else if (topicName == categories.backendDev) { setBackendDev(!backendDev) }
+    else if (topicName == categories.webDev) { setWebDev(!webDev) }
+    else if (topicName == categories.mobileDev) { setMobileDev(!mobileDev) }
+    else if (topicName == categories.gameDev) { setGameDev(!gameDev) }
+    else if (topicName == categories.embeddedSystems) { setEmbeddedSystems(!embeddedSystems) }
+    else if (topicName == categories.algorithms) { setAlgorithms(!algorithms) }
+    else if (topicName == categories.softwareOptimization) { setSoftwareOptimization(!softwareOptimization) }
+    else if (topicName == categories.security) { setSecurity(!security) }
+    else if (topicName == categories.blockchain) { setBlockchain(!blockchain) }
+  };
+
   return (
     <TouchableOpacity
       style={styles.container}
-      onPress={() => handleShowImage()}
+      onPress={() => {
+        handleShowImage()
+        handleSetCategory()
+      }}
     >
       <ImageBackground
         source={{ uri: `${imageURL}` }}
