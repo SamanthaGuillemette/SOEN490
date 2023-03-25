80885d7d6a2c3dbcd0ee8760c04278cca90104b8
Laila Alhalabi <laila.alhalabi99@gmail.com>
2023-03-24 23:15:44 -0400

    XP #436: useListNotifications function created

--- Xplore/services/api/notifications.ts
+++ Xplore/services/api/notifications.ts
@@ -4,6 +4,16 @@ import { Query } from "appwrite";
 import api from "../appwrite/api";
 
 // notificationType should be "badge" | "groupAdd" | "joinRequest" | "joinAccept"
+interface Notifications {
+  notificationType: any;
+  createdAt: any;
+  groupName: any;
+  chatID: any;
+  projectID: any;
+  memberRequestingID: any;
+  memberAcceptedRequestID: any;
+  badgeName: any;
+}
 
 const createBadgeNotif = async (userID: any, badgeName: any) => {
   await api.createDocument(COLLECTION_ID_NOTIFICATIONS, {
@@ -48,9 +58,59 @@ const createAcceptJoinNotif = async (userID: any, projectID: any) => {
   });
 };
 
+const getNotifs = async (userID: any) => {
+  const response = await api.listDocuments(COLLECTION_ID_NOTIFICATIONS, [
+    Query.equal("userID", userID),
+  ]);
+  const notifs = await Promise.all(
+    response?.documents?.map(async (doc) => {
+      return {
+        //for all notifs
+        notificationType: doc.notificationType,
+        createdAt: doc.createdAt,
+
+        //for group chats add
+        groupName: doc.groupName,
+        chatID: doc.chatID,
+
+        //for project joining
+        projectID: doc.projectID,
+        memberRequestingID: doc.memberRequestingID,
+        memberAcceptedRequestID: doc.memberAcceptedRequestID,
+
+        //for badges
+        badgeName: doc.badgeName,
+      };
+    })
+  );
+  return notifs;
+};
+
+const useListNotifications = async (userID: any) => {
+  const [notifs, setNotifs] = useState<Notifications[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const notifications = await getNotifs(userID);
+        notifications.sort(
+          (notif1: any, notif2: any) =>
+            new Date(notif2.lastModifiedAt).getTime() -
+            new Date(notif1.lastModifiedAt).getTime()
+        );
+        setNotifs(notifications);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [userID]);
+  return notifs;
+};
+
 export {
   createBadgeNotif,
   createGroupAddNotif,
   createRequestJoinNotif,
   createAcceptJoinNotif,
+  useListNotifications,
 };
