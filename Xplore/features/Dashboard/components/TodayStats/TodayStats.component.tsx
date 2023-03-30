f8e379885cd772811463261d11b0c6802aee9938
Samantha Guillemette <samantha.guillemette@gmail.com>
2023-03-30 09:32:15 -0400

    XP #448: added in progress projects stat

--- Xplore/features/Dashboard/components/TodayStats/TodayStats.component.tsx
+++ Xplore/features/Dashboard/components/TodayStats/TodayStats.component.tsx
@@ -1,9 +1,20 @@
+import { useQuery } from "react-query";
 import { Text, View } from "../../../../components";
+import { useProjectCardInfo } from "../../../../services/api/projects";
 import { StatBoxLarge } from "../StatBoxLarge";
 import { StatBoxSmall } from "../StatBoxSmall";
 import styles from "./TodayStats.styles";
+import api from "../../../../services/appwrite/api";
 
 export const TodayStats = () => {
+  var allProjectCard: any;
+
+  // Quering current user's data
+  const { data: userdata } = useQuery("user data", () => api.getAccount());
+  let userId: string = userdata?.$id as string;
+
+  allProjectCard = useProjectCardInfo(userId);
+
   return (
     <View style={styles.container}>
       <Text variant="h2" color="titleText" style={styles.todayTitle}>
@@ -13,7 +24,7 @@ export const TodayStats = () => {
         <View style={styles.leftPanel}>
           <StatBoxLarge
             title="In Progress"
-            subTitle="3 Projects"
+            subTitle={allProjectCard[0]?.length + " Projects"}
             iconName="refresh-ccw"
           />
           <StatBoxSmall
