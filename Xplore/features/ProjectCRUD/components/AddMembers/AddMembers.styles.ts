a90a758935099f40bcf50156cfd0e072d8d455ae
Laila Alhalabi <laila.alhalabi99@gmail.com>
2023-03-31 04:03:44 -0400

    [hotfix] fixed height of add modal

--- Xplore/features/ProjectCRUD/components/AddMembers/AddMembers.styles.ts
+++ Xplore/features/ProjectCRUD/components/AddMembers/AddMembers.styles.ts
@@ -3,12 +3,12 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   scrollView: {
     marginTop: 10,
-    minHeight: 200,
+    minHeight: 300,
   },
   fixedHeight: {
     justifyContent: "center",
-    maxHeight: 300,
-    minHeight: 300,
+    maxHeight: 400,
+    minHeight: 400,
   },
   searchBar: {
     width: 280,
