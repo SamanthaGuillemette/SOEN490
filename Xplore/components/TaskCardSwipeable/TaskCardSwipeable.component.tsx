0074596c177ff0b5ef6de2938100ff3d3c3f345d
Samantha Guillemette <samantha.guillemette@gmail.com>
2023-03-31 15:48:22 -0400

    [Refactor]: removed taskcontent since now using TaskCard

--- Xplore/components/TaskCardSwipeable/TaskCardSwipeable.component.tsx
+++ Xplore/components/TaskCardSwipeable/TaskCardSwipeable.component.tsx
@@ -1,5 +1,5 @@
 import { View } from "../View";
-import { Text } from "../Text";
+
 import { Icon } from "../Icon";
 import { TouchableOpacity } from "react-native";
 import { ListItem } from "@rneui/themed";
@@ -39,38 +39,6 @@ export const actions = (props: any) => {
   );
 };
 
-export const taskContent = (props: taskContentProps) => {
-  const task = props.taskInfo;
-
-  return (
-    <View backgroundColor="backgroundSecondary" style={styles.innerBox}>
-      <TouchableOpacity
-        onPress={() =>
-          props.navigation.navigate("IndividualTask", {
-            taskInfo: task,
-          })
-        }
-      >
-        <View style={styles.taskContentContainer}>
-          <Text style={styles.taskCategory} variant="h4" color="linkText">
-            {task.category}
-          </Text>
-
-          <Icon style={styles.taskInfoIcon} size="medium" name="help-circle" />
-        </View>
-        <Text style={styles.taskName} variant="h2" color="titleText">
-          {task.name}
-        </Text>
-        <View style={styles.dateContainer}>
-          <Icon style={styles.calenderIcon} size="medium" name="calendar" />
-          <Text style={styles.date} variant="smBody" color="bodyText">
-            {task.endDate.substring(0, task.endDate.indexOf("T"))}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
-};
 export const TaskCardSwipeable = (props: taskContentProps) => {
   const generalGray = useThemeColor("generalGray");
 
