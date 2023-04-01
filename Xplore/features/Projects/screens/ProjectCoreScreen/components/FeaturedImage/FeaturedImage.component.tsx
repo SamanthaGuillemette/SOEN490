9720c4e7469f0a9c1cb107fa3ed6d7822885efa5
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2023-03-31 22:48:53 -0400

    XP #452: Added default image to to ProjectDetails

--- Xplore/features/Projects/screens/ProjectCoreScreen/components/FeaturedImage/FeaturedImage.component.tsx
+++ Xplore/features/Projects/screens/ProjectCoreScreen/components/FeaturedImage/FeaturedImage.component.tsx
@@ -5,61 +5,59 @@ import { useColorScheme } from "../../../../../../hooks";
 
 interface ShadowImageProps {
   projectName: string;
-  source: string;
+  source?: string | null;
 }
 
 export const FeaturedImage = (props: ShadowImageProps) => {
   const { projectName, source } = props;
   const colorScheme = useColorScheme();
 
+  const imageSource =
+    source || require("../../../../../../assets/logo_dark.png");
+
   return (
     <ShadowView backgroundColor="primary" style={styles.container}>
-      {colorScheme === "dark" ? (
-        source !== "" || source !== null ? (
-          <ImageBackground
-            source={{
-              uri: source,
-            }}
-            imageStyle={styles.imageBgContainer}
-          >
-            <View style={styles.thirdContainer}>
-              <RNView style={styles.overlay} />
-              <Text variant="h3" style={[styles.whiteText, styles.projectName]}>
-                {projectName}
-              </Text>
-            </View>
-          </ImageBackground>
-        ) : (
-          <ImageBackground
-            imageStyle={styles.imageBgContainer}
-            source={require("../../../../../../assets/logo_dark.png")}
-          >
-            <View style={styles.thirdContainer}>
-              <RNView style={styles.overlay} />
-              <Text variant="h3" style={[styles.whiteText, styles.projectName]}>
-                {projectName}
-              </Text>
-            </View>
-          </ImageBackground>
-        )
-      ) : source !== "" || source !== null ? (
-        <ImageBackground
-          source={{
-            uri: source,
-          }}
-          imageStyle={styles.imageBgContainer}
-        >
-          <View style={styles.thirdContainer}>
-            <RNView style={styles.overlay} />
-            <Text variant="h3" style={[styles.whiteText, styles.projectName]}>
-              {projectName}
-            </Text>
-          </View>
-        </ImageBackground>
-      ) : (
+      {colorScheme === "dark"
+        ? source && (
+            <ImageBackground
+              source={{
+                uri: source,
+              }}
+              imageStyle={styles.imageBgContainer}
+            >
+              <View style={styles.thirdContainer}>
+                <RNView style={styles.overlay} />
+                <Text
+                  variant="h3"
+                  style={[styles.whiteText, styles.projectName]}
+                >
+                  {projectName}
+                </Text>
+              </View>
+            </ImageBackground>
+          )
+        : source && (
+            <ImageBackground
+              source={{
+                uri: source,
+              }}
+              imageStyle={styles.imageBgContainer}
+            >
+              <View style={styles.thirdContainer}>
+                <RNView style={styles.overlay} />
+                <Text
+                  variant="h3"
+                  style={[styles.whiteText, styles.projectName]}
+                >
+                  {projectName}
+                </Text>
+              </View>
+            </ImageBackground>
+          )}
+      {!source && (
         <ImageBackground
           imageStyle={styles.imageBgContainer}
-          source={require("../../../../../../assets/logo_light.png")}
+          source={imageSource}
         >
           <View style={styles.thirdContainer}>
             <RNView style={styles.overlay} />
